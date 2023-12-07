473783d5aecfbaa6f172140c95dc2556650c334d
Felipe Cordeiro <imfelipe.cordeiro12@gmail.com>
2023-12-06 22:46:43 -0300

    Project Angular and Flask

---
+++ src/app/person/person.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent implements OnInit{
+  name = 'Felipe';
+  ngOnInit(): void {
+
+  }
+}
